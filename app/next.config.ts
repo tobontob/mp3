f36023851762104a46bd4b0c4f8422c3db8ee95e
tobontob <tobontob@naver.com>
2025-06-27 19:59:08 +0900

    all up

---
+++ app/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
